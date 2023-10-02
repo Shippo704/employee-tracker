9d80eac0e94ef7d032360c4b029fed4888946845
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-02 19:33:54 -0400

    added functionality to final ADD function

--- db.js
+++ db.js
@@ -56,6 +56,7 @@ function addDepartment() {
 };
 
 function addRole() {
+    // get departments from database
     const query = "SELECT * FROM department";
     db.query(query, (error, results) => {
         if (error) console.log(error);
@@ -80,15 +81,17 @@ function addRole() {
             )
         }
     ])
+    // add department to the new role object
     .then((newRole) => {
         const department = results.find((department) => {
             department.name === newRole.department
         });
+        // insert new role into role table
         const query = "INSERT INTO role SET ?";
         db.query(query, {
             title: newRole.title,
             salary: newRole.salary,
-            department_id: department
+            department_id: department.id
         }, 
         (error, results) => {
             if (error) console.log(error);
@@ -98,11 +101,73 @@ function addRole() {
 
 };
 
-async function addEmployee() {
+function addEmployee() {
+    // get roles from database
+    db.query("SELECT id, title from roles", (error, results) =>{
+        if (error) return console.log(error);
+        const roles = results.map(({id, title}) => ({
+            name: title,
+            value: id
+        }));
+        
+        // get employee list for managers
+        db.query("SELECT id, CONCAT(first_name, ' ', last_name) as name FROM employee", (error, results) => {
+            if (error) return console.log(error);
+            const managers = results.map(({id, name}) => ({
+                name: name,
+                value: id
+            }));
+
+            // employee information
+            inquirer.prompt([
+                {
+                    type: input,
+                    name: "first_name",
+                    message: "Enter the new employee's first name: "
+                },
+                {
+                    type: input,
+                    name: "last_name",
+                    messsge: "Enter the new employee's last name: "
+                },
+                {
+                    type: list,
+                    name: "role_id",
+                    message: "Select the new employee's role:",
+                    choices: [
+                        {name: "None", value: null},
+                        ...roles
+                    ]
+                },
+                {
+                    type: list,
+                    name: "manager_id",
+                    message: "Select the new employee's manager:",
+                    choices: [
+                        {name: "None", value: null},
+                        ...managers
+                    ]
+                }
+            ])
+            .then((newEmployee) => {
+                const query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+                const values = [
+                    newEmployee.first_name,
+                    newEmployee.last_name,
+                    newEmployee.role_id,
+                    newEmployee.manager_id
+                ];
+                db.query(query, values, (error) => {
+                    if (error) return console.log(error);
+                    console.log("New employee added successfully.");
+                })
+            })
+        })
+    });
 
 };
 
-async function updateEmployeeRole() {
+function updateEmployeeRole() {
 
 };
 
